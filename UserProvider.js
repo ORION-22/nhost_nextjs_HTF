b3467f5cefc2c512118c61a561590eef60badfdb
ONASVEEBANARSE <obanarse@gmail.com>
2022-12-10 12:05:27 +0530

    firstcommit

--- UserProvider.js
+++ UserProvider.js
@@ -1,14 +1,37 @@
-import React, { useContext } from 'react';
+import React from "react";
+import { useUserId  } from "@nhost/nextjs";
+import { gql, useQuery } from "@apollo/client";
 
-const UserContext = React.createContext(null);
+const UserContext = React.createContext(null)
+
+const GET_USER_QUERY = gql`
+  query GetUser($id: uuid!) {
+    user(id: $id) {
+      id
+      email
+      displayName
+      metadata
+      avatarUrl
+    }
+  }
+`
 
 export function UserProvider({ children = null }) {
-  const user = null;
+  const id = useUserId();
+  const { loading, error, data } = useQuery(GET_USER_QUERY, {
+    variables: { id },
+    skip: !id,
+  });
+  const user = data?.user;
+
+  if (error) {
+    return <p>Something went wrong. Try to refresh the page.</p>;
+  }
+  if (loading) {
+    return null;
+  }
+
   return (
     <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
   );
-}
-
-export function useUserContext() {
-  return useContext(UserContext);
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,27 @@
-import '../styles/globals.css';
-import { Toaster } from 'react-hot-toast';
-import { UserProvider } from '../UserProvider';
+import "../styles/globals.css";
+import { nhost } from "./nhost";
+import { Toaster } from "react-hot-toast";
+import { UserProvider } from "../UserProvider";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { NhostNextProvider } from "@nhost/nextjs";
+import { NhostApolloProvider } from "@nhost/react-apollo";
+
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-      <Toaster />
-    </UserProvider>
+    <NhostNextProvider nhost={nhost} initial={pageProps.nhostSession}>
+      <NhostApolloProvider nhost={nhost}>{/* ... */}</NhostApolloProvider>
+      <ApolloProvider client={client}>
+        <UserProvider>
+          <Component {...pageProps} />
+          <Toaster />
+        </UserProvider>
+      </ApolloProvider>
+    </NhostNextProvider>
   );
 }
 
